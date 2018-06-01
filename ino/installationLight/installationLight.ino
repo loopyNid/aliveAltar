4c3f7717114b714be4a8318e73953fc1b2e0cc69
nikk charalampidis <nikk.ch@protonmail.com>
2018-06-01 21:27:47 +0300

    fixes arduino loop

--- ino/installationLight/installationLight.ino
+++ ino/installationLight/installationLight.ino
@@ -3,16 +3,18 @@
 
 #define trigPin1 3 // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define echoPin1 2  // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 400 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define MAX_DISTANCE 480 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 #define trigPin2 5
 #define echoPin2 4
 #define trigPin3 11
 #define echoPin3 10
 #define trigPin4 13
 #define echoPin4 12
+int ledChange = 170;
 int led = 9;
+int ledW = 6;
 int brightness = 0;
-int fadeAmount = 4;
+int fadeAmount = 5;
 
 NewPing sonar1(trigPin1, echoPin1, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 NewPing sonar2(trigPin2, echoPin2, MAX_DISTANCE);
@@ -22,12 +24,14 @@ NewPing sonar4(trigPin4, echoPin4, MAX_DISTANCE);
 void setup() {
   Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
   pinMode(led, OUTPUT);
+  pinMode(ledW, OUTPUT);
  
 }
 
 void loop() {
-
+//  Serial.println(brightness);
   analogWrite(led, brightness);
+  analogWrite(ledW, 255 - brightness);
   
   delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
 
@@ -46,20 +50,23 @@ void loop() {
   else{
   Serial.print("US1:");
   Serial.println(uS1RNDT); // Send ping, get distance in cm and print result (0 = outside set distance range)
+
  
-  if(((uS1RNDT < 70) && (uS1RNDT > 2)) || ((uS2RNDT < 70) && (uS2RNDT > 2))||((uS3RNDT < 70) && (uS3RNDT > 2)) || ((uS4RNDT < 70) && (uS4RNDT > 2))){
-     if(brightness >= 245){
-       brightness = 255;
-     } else {
-       brightness = brightness + fadeAmount;
-     }
-  }else if((uS1RNDT > 70) || (uS2RNDT > 70)|| (uS3RNDT > 70) || (uS4RNDT > 70)){
-     if( brightness <= 10 ){
-       brightness = 0;
-     } else {
-      brightness =  brightness - fadeAmount;
-     }
-  }
+    if(((uS1RNDT < ledChange) && (uS1RNDT > 2)) || ((uS2RNDT < ledChange) && (uS2RNDT > 2))||((uS3RNDT < ledChange) && (uS3RNDT > 2)) || ((uS4RNDT < ledChange) && (uS4RNDT > 2))){
+//      Serial.println('+');
+       if(brightness >= 245){
+         brightness = 255;
+       } else {
+         brightness = brightness + fadeAmount;
+       }
+    }else if((uS1RNDT >= ledChange)|| (uS2RNDT >= ledChange)|| (uS3RNDT >= ledChange) || (uS4RNDT >= ledChange)){
+//      Serial.println('-');
+       if( brightness <= 10 ){
+         brightness = 0;
+       } else {
+        brightness =  brightness - fadeAmount;
+       }
+    }
   }
   if(uS2RNDT == 0){
   
@@ -67,6 +74,21 @@ void loop() {
   else{
   Serial.print("US2:");
   Serial.println(uS2RNDT);
+     if(((uS2RNDT < ledChange))){
+//      Serial.println('+');
+       if(brightness >= 245){
+         brightness = 255;
+       } else {
+         brightness = brightness + fadeAmount;
+       }
+    }else if((uS2RNDT >= ledChange)){
+//      Serial.println('-');
+       if( brightness <= 10 ){
+         brightness = 0;
+       } else {
+        brightness =  brightness - fadeAmount;
+       }
+    }
   
   }
   if(uS3RNDT == 0){
@@ -75,14 +97,44 @@ void loop() {
   else{
   Serial.print("US3:");
   Serial.println(uS3RNDT);
+     if(((uS3RNDT < ledChange))){
+//      Serial.println('+');
+       if(brightness >= 245){
+         brightness = 255;
+       } else {
+         brightness = brightness + fadeAmount;
+       }
+    }else if((uS3RNDT >= ledChange)){
+//      Serial.println('-');
+       if( brightness <= 10 ){
+         brightness = 0;
+       } else {
+        brightness =  brightness - fadeAmount;
+       }
+    }
   
   }
-   if(uS3RNDT == 0){
+   if(uS4RNDT == 0){
   
   }
   else{
   Serial.print("US4:");
   Serial.println(uS4RNDT);
+     if(((uS4RNDT < ledChange))){
+//      Serial.println('+');
+       if(brightness >= 245){
+         brightness = 255;
+       } else {
+         brightness = brightness + fadeAmount;
+       }
+    }else if((uS4RNDT >= ledChange)){
+//      Serial.println('-');
+       if( brightness <= 10 ){
+         brightness = 0;
+       } else {
+        brightness =  brightness - fadeAmount;
+       }
+    }
   
   }
 
